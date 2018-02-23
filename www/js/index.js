c6d965390f51a0256453a37c25d0de3a4f959403
Tiago Neves anhaabaete <anhaabaete@gmail.com>
2018-02-23 18:14:29 -0300

    finalizacao

---
+++ www/js/index.js
@@ -0,0 +1,16 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+   onDeviceReady: function() {
+        
+    },
+    
+    esperaCarregar: function() {
+        
+    }
+};
+
+app.initialize();
